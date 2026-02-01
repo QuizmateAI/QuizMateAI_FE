3dc28332f348f4887521afaff4eacbd88b1404bb
Trương Bảo Ngọc <ngoctbse183713@fpt.edu.vn>
2026-02-01 16:24:18 +0700

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import HeroSection from './Hero';
+import HeroSection from './Pages/LandingPage';
 import './App.css'
 
 function App() {
